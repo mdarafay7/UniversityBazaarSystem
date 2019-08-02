6f7976c3209b8746d594a1c65a878299bd6cb4c9
mdarafay <mdarafay@gmail.com>
2019-08-01 23:50:11 -0500

    Runnable

--- app/src/main/java/com/example/ubs/Messaging.java
+++ app/src/main/java/com/example/ubs/Messaging.java
@@ -1,62 +1,31 @@
 package com.example.ubs;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
 
-import com.google.android.material.tabs.TabLayout;
+public class Messaging extends AppCompatActivity {
 
-public class Messaging extends AppCompatActivity implements TabLayout.OnTabSelectedListener{
-    private TabLayout tabLayout;
-
-    //This is our viewPager
-    private ViewPager viewPager;
+    private Button single;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Adding toolbar to the activity
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        single=findViewById(R.id.Single);
 
-        //Initializing the tablayout
-        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
-
-        //Adding the tabs using addTab() method
-        tabLayout.addTab(tabLayout.newTab().setText("Tab1"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab2"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab3"));
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-
-        //Initializing viewPager
-        viewPager = (ViewPager) findViewById(R.id.pager);
-
-        //Creating our pager adapter
-        Pager adapter = new Pager(getSupportFragmentManager(), tabLayout.getTabCount());
-
-        //Adding adapter to pager
-        viewPager.setAdapter(adapter);
-
-        //Adding onTabSelectedListener to swipe views
-        tabLayout.setOnTabSelectedListener(this);
+        single.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Messaging.this, UsrMsg.class);
+                startActivity(intent);
+            }
+        });
     }
-
-    @Override
-    public void onTabSelected(TabLayout.Tab tab) {
-        viewPager.setCurrentItem(tab.getPosition());
-    }
-
-    @Override
-    public void onTabUnselected(TabLayout.Tab tab) {
-
-    }
-
-    @Override
-    public void onTabReselected(TabLayout.Tab tab) {
-
-    }
+    //TextView myAwesomeTextView = (TextView)findViewById(R.id.textView2);
+    //myAwesomeTextView.setText("University Bazaar System");
 }
---
+++ app/src/main/java/com/example/ubs/UsrMsg.java
@@ -0,0 +1,24 @@
+package com.example.ubs;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+    public class UsrMsg extends AppCompatActivity {
+
+        private Button Clubs;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.tab1);
+
+        }
+        //TextView myAwesomeTextView = (TextView)findViewById(R.id.textView2);
+        //myAwesomeTextView.setText("University Bazaar System");
+    }
+
+
